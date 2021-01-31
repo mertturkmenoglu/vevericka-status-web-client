31d01d6fd37a0d6173141f2f22ceb0699b4651b2
Mert <mertturkmenoglu99@gmail.com>
2021-01-31 04:48:49 +0300

    Add files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://vevericka.herokuapp.com/">
+          <img
+            src="https://raw.githubusercontent.com/mertturkmenoglu/vevericka-web-client/d9f9dc18be8a868c1345b375110fdf0452bf059c/src/assets/icon_primary.svg"
+            width="28"
+            height="28"
+          />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="https://vevericka.herokuapp.com/">
+            Vevericka
+          </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link"> More </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item"> About </a>
+              <a class="navbar-item"> Contact </a>
+              <hr class="navbar-divider" />
+              <a
+                class="navbar-item"
+                href="https://github.com/mertturkmenoglu/vevericka-web-client/issues"
+              >
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a
+                class="button is-dark"
+                href="https://github.com/mertturkmenoglu/vevericka-web-client"
+              >
+                <strong>GitHub</strong>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="hero is-success is-small mt-3">
+      <div class="hero-body has-text-centered">
+        <p class="title has-text-weight-light">Vevericka Status Service</p>
+      </div>
+    </section>
+
+    <div v-if="!loading" class="mt-5">
+      <div v-for="s in statuses" :key="s.name">
+        <div class="container">
+          <Service :service="s" class="mb-3" />
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <b-loading v-model="loading" :can-cancel="false"></b-loading>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Service from "@/components/Service";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Service },
+  data: () => ({
+    URL: "https://bubbly-mantis-303320.ey.r.appspot.com/api/v1/statuses",
+    statuses: [],
+    loading: true,
+  }),
+  methods: {
+    async fetchStatuses() {
+      const response = await fetch(this.URL);
+      const { data } = await response.json();
+      this.statuses = data.statuses;
+    },
+  },
+  mounted() {
+    this.fetchStatuses().then(() => (this.loading = false));
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Service.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card-bg card has-text-success">
+    <div class="card-content">
+      <div>
+        <span class="title has-text-success is-size-4 has-text-weight-light">
+          {{ service.name }}
+        </span>
+        <span
+          class="title has-text-success is-pulled-right is-size-4 has-text-weight-light"
+        >
+          {{ operational }}
+        </span>
+      </div>
+      <div class="subtitle has-text-success has-text-weight-light">
+        Status: {{ service.status_code }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Service",
+  props: ["service"],
+  data: () => ({}),
+  computed: {
+    operational() {
+      return this.service.status_code === 200
+        ? "Operational"
+        : "Not Operational";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-bg {
+  background-color: rgb(255, 255, 255) !important;
+}
+</style>
